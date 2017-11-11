a77ecfba71f502c50f93fbe1df45eb7ba5ead9b7
janaleible <jana.leible@web.de>
2017-11-11 20:52:55 +0100

    WIP

--- my_driver.py
+++ my_driver.py
@@ -1,11 +1,27 @@
 from pytocl.driver import Driver
 from pytocl.car import State, Command
 
+import pandas
+import torch
+from pandas.core.frame import DataFrame
+from torch import nn, optim
+import torch.nn.functional as Functional
+from torch.autograd import Variable
+
+from scratch import CarControllerNet, TrainingData
+
 
 class MyDriver(Driver):
-    # Override the `drive` method to create your own driver
-    ...
-    # def drive(self, carstate: State) -> Command:
-    #     # Interesting stuff
-    #     command = Command(...)
-    #     return command
+
+    def __init__(self):
+        super(MyDriver, self).__init__()
+        self.net = CarControllerNet()
+        self.net.train(
+            2,
+            optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9),
+            TrainingData(pandas.read_csv('training-data/train_data/aalborg.csv'))
+        )
+
+    def drive(self, carstate: State) -> Command:
+
+        return self.net.predict(carstate)
--- scratch.py
+++ scratch.py
@@ -5,6 +5,8 @@ from torch import nn, optim
 import torch.nn.functional as Functional
 from torch.autograd import Variable
 
+from pytocl.car import State, Command
+
 targetColumns = ['ACCELERATION', 'BRAKE', 'STEERING']
 dataColumns = ['SPEED', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS'] + ['TRACK_EDGE_' + str(i) for i in range(19)]
 
@@ -45,38 +47,57 @@ class Net(nn.Module):
 
         return x
 
-net = Net(22, 30, 3)
+class CarControllerNet():
 
-optimiser = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    def __init__(self):
+        self.trained = False
+        self.net = Net(22, 30, 3)
 
-for epoch in range(numberOfEpochs):
+    def train(self, numberOfEpochs, optimiser, trainingData: TrainingData):
 
-    runningLoss = 0.0
+        for epoch in range(numberOfEpochs):
 
-    train = TrainingData(aalborg_raw)
-    for i in range(len(train)):
+            for i in range(len(trainingData)):
+                data = trainingData[i]
 
-        data = train[i]
+                inputs, targets = data
 
-        inputs, targets = data
+                inputs = Variable(inputs)
+                targets = Variable(targets)
 
-        inputs = Variable(inputs)
-        targets = Variable(targets)
+                optimiser.zero_grad()
 
-        optimiser.zero_grad()
+                outputs = self.net(inputs)
+                loss = Functional.l1_loss(outputs, targets)
+                loss.backward()
+                optimiser.step()
 
-        outputs = net(inputs)
-        loss = Functional.l1_loss(outputs, targets)
-        loss.backward()
-        optimiser.step()
+            self.trained = True
 
-        runningLoss += loss.data[0]
+    def predict(self, input: State) -> Command:
 
-        if(i % 2000 == 1999):
-            print('[%d, %5d] loss: %.3f' % (
-                epoch + 1,
-                i + 1,
-                runningLoss / 2000
-            ))
+        if(not self.trained): raise NotImplementedError
 
-print('Done training')
+        prediction = self.net(Variable(input))
+
+        command = Command()
+        command.accelerator = prediction[0].data.numpy()[0]
+        command.brake = prediction[1].data.numpy()[0]
+        command.steering = prediction[2].data.numpy()[0]
+        command.gear = 1
+
+        return command
+
+    def parameters(self):
+        return self.net.parameters()
+
+net = CarControllerNet()
+net.train(
+    2,
+    optim.SGD(net.parameters(), lr=0.001, momentum=0.9),
+    TrainingData(pandas.read_csv('training-data/train_data/aalborg.csv'))
+)
+
+prediction = net.predict(torch.FloatTensor([-0.0379823, -5.61714E-5, 4.30409E-4, 5.00028, 5.0778, 5.32202, 5.77526, 6.52976, 7.78305, 10.008, 14.6372, 28.8659, 200.0, 28.7221, 14.6009, 9.99199, 7.7742, 6.52431, 5.77175, 5.31976, 5.07646, 4.99972]))
+
+print(prediction)

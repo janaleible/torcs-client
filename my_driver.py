b0faacda7091c1cb4a3299837f45589886ef92a3
janaleible <jana.leible@web.de>
2017-11-15 12:20:59 +0100

    use network for driving

--- my_driver.py
+++ my_driver.py
@@ -1,9 +1,13 @@
+import torch
+from torch.autograd import Variable
+
 from pytocl.driver import Driver
 from pytocl.car import State, Command
 
+from models.basicnetwork import Net
+
 
 class MyDriver(Driver):
-    # Override the `drive` method to create your own driver
 
     timeSinceLastShift = 0
 
@@ -17,10 +21,27 @@ class MyDriver(Driver):
 
         command = Command()
 
-        command.accelerator = 1
+        net = Net(21, 30, 3)
+        net.load_state_dict(torch.load('models/models/11151134.model'))
+
+        prediction = net(self.stateToSample(carstate))
+
+        command.accelerator = self.predictionToFloat(prediction[0])
+        command.brake = self.predictionToFloat(prediction[1])
+        command.steering = self.predictionToFloat(prediction[2])
+
         command.gear = self.shiftGears(carstate.gear, carstate.rpm)
 
         return command
+
+    def predictionToFloat(self, prediction: Variable) -> float:
+        return prediction.data.numpy()[0]
+
+    def stateToSample(self, state: State) -> Variable:
+
+        sample = [state.speed_x, state.distance_from_center, state.angle] + [distance for distance in state.distances_from_edge]
+
+        return Variable(torch.FloatTensor(sample[0:-1]))
 
     def shiftGears(self, previousGear: int, rpm: float) -> int:
 

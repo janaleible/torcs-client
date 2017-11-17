2b5a7e78afd4a8792e1439f9be04336665b94913
Bence Keresztury <kbence16@gmail.com>
2017-11-17 13:38:03 +0100

    Deleted the last lines from the other two training files Normalized the input in all 3 files

---
+++ training-data/train_data/normalizing_input.py
@@ -0,0 +1,35 @@
+import numpy as np
+import csv
+
+
+
+file_names = ["aalborg", "alpine-1", "f-speedway"]
+
+
+for i in range(len(file_names)):
+    with open("/home/student/Documents/torcs-server/torcs-client//training-data/train_data/" + file_names[i]+".csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)
+        data_list = []
+        for row in reader:
+            data_list.append(row)
+
+        num_row = len(data_list)
+        num_col = len(data_list[0])
+        data_mtx = np.zeros((num_row, num_col))
+
+
+        for j in range(num_row):
+            for k in range(num_col):
+                data_mtx[j][k] = data_list[j][k]
+
+
+        column_means = np.zeros(num_col)
+        column_means = np.sum(data_mtx, axis=0)/(num_row)
+        column_std = data_mtx.std(0)
+
+
+        for l in range(3,num_col):
+            data_mtx[:,l] = (data_mtx[:,l]-column_means[l])/column_std[l]
+
+        np.savetxt(file_names[i]+"normalized.csv", data_mtx, delimiter=",")

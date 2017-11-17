50c5f5d76e82d7063ebfba041a02ab8795be2471
Bence Keresztury <kbence16@gmail.com>
2017-11-17 14:20:08 +0100

    Headers added back to normalized data

--- training-data/train_data/normalizing_input.py
+++ training-data/train_data/normalizing_input.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import pandas as pd
+
 
 
 
@@ -18,11 +20,9 @@ for i in range(len(file_names)):
         num_col = len(data_list[0])
         data_mtx = np.zeros((num_row, num_col))
 
-
         for j in range(num_row):
             for k in range(num_col):
                 data_mtx[j][k] = data_list[j][k]
-
 
         column_means = np.zeros(num_col)
         column_means = np.sum(data_mtx, axis=0)/(num_row)
@@ -32,4 +32,8 @@ for i in range(len(file_names)):
         for l in range(3,num_col):
             data_mtx[:,l] = (data_mtx[:,l]-column_means[l])/column_std[l]
 
-        np.savetxt(file_names[i]+"normalized.csv", data_mtx, delimiter=",")
+        df = pd.DataFrame(data_mtx, columns = header)
+        df.to_csv(file_names[i]+"normalized.csv", index = False, header = True, sep = ",")
+
+        # np.savetxt(file_names[i]+"normalized.csv", data_mtx, delimiter=",")
+
